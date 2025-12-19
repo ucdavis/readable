64d91d301d2160eab8a1e5d1bdd0fcb85460a1bf
Scott Kirkland <srkirkland@gmail.com>
2025-12-19 13:28:53 -0800

    queue process placeholder

---
+++ workers/function.ingest/ProcessQueueMessage.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace function.ingest;
+
+public class ProcessQueueMessage
+{
+    private readonly ILogger<ProcessQueueMessage> _logger;
+
+    public ProcessQueueMessage(ILogger<ProcessQueueMessage> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(ProcessQueueMessage))]
+    public async Task Run(
+        [ServiceBusTrigger("myqueue", Connection = "")]
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        _logger.LogInformation("Message ID: {id}", message.MessageId);
+        _logger.LogInformation("Message Body: {body}", message.Body);
+        _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+        // Complete the message
+        await messageActions.CompleteMessageAsync(message);
+    }
+}
