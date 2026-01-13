2c16b58c1fe409f652ce18ffc9d74db49858f89b
Cal <cydoval@mac.com>
2026-01-13 08:28:49 -0800

    footer work

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -2,13 +2,19 @@ import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { RouterContext } from '../main.tsx';
+import Footer from '@/components/pdf/Footer.tsx';
 
 const RootLayout = () => (
-  <>
-    <Outlet />
+  <div className="min-h-screen bg-base-200 flex flex-col">
+    <main className="flex-1">
+      <Outlet />
+    </main>
+
+    <Footer />
+
     <ReactQueryDevtools buttonPosition="top-right" />
     <TanStackRouterDevtools position="bottom-right" />
-  </>
+  </div>
 );
 
 export const Route = createRootRouteWithContext<RouterContext>()({
